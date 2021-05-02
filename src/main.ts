fdc211111c1740c60ef72d02f8b3186c40f925d0
Mert Oksuz <mert.o@onthegosystems.com>
2021-05-03 00:58:03 +0300

    remove token

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function main() {
 
     // init bot
     const bot = new Client();
-    bot.login('NzkxNjA1OTczMjYwNDM1NDY3.X-Rmgw.XTFQrMDz75Q7soRq2KZ27t8-esc');
+    bot.login(process.env.TOKEN);
 
     // init pre-reqs
     await DAL.init();
